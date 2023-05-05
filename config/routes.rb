cbc2a4e0bd2ac82954e60807f41f66e6c2a847d0
houdamzari <houda.mzari1999@gmail.com>
2023-05-05 23:38:41 +0100

    init

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :messages, only: %i[index show] do
-        collection { get :random }
-      end
-    end
-  end
-end
